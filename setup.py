51df2b5a27d648e4bc6c2c2b9c5f8e67e21fc20c
borni <borniafsanasadia@gmail.com>
2025-01-21 21:54:41 +0900

    init file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Doctor Chatbot',
+    version= '0.0.0',
+    author= 'Abdulla Al Mobassir And Afsana Sadia Borni',
+    author_email= 'aamobassir34@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
